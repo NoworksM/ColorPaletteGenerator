93e75eea65b7628294f9a3b4c1cef5e60cc82099
Mike Nowak <nowakmikea@gmail.com>
2024-07-05 00:07:56 -0500

    Added hue display underneath hue editor for easier reference

--- src/components/color-hue-editor.tsx
+++ src/components/color-hue-editor.tsx
@@ -1,7 +1,10 @@
 import RangeSlider from './input/range-slider.tsx'
 
 export default function ColorHueEditor({hue, onHueChanged}: ColorHueEditorParams) {
-    return <RangeSlider label="Hue" min={0} max={360} step={0.001} value={hue} decimalPlaces={3} onValueChanged={onHueChanged}/>
+    return <div className="flex flex-col w-64">
+        <RangeSlider label="Hue" min={0} max={360} step={0.001} value={hue} decimalPlaces={3} onValueChanged={onHueChanged}/>
+        <div className="h-6" style={{backgroundImage: `linear-gradient(to right, oklch(74% 0.12 0), oklch(74% 0.12 30), oklch(74% 0.12 60), oklch(74% 0.12 90), oklch(74% 0.12 120), oklch(74% 0.12 150), oklch(74% 0.12 180), oklch(74% 0.12 210), oklch(74% 0.12 240), oklch(74% 0.12 270), oklch(74% 0.12 300), oklch(74% 0.12 330), oklch(74% 0.12 360)`}}/>
+    </div>
 }
 
 export interface ColorHueEditorParams {
