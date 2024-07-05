5c59e90b0383c0d9adc35eede0cc8107136f4a47
Mike Nowak <nowakmikea@gmail.com>
2024-07-05 00:40:33 -0500

    Added checkbox.tsx

---
+++ src/components/input/checkbox.tsx
@@ -0,0 +1,24 @@
+import {ChangeEvent} from 'react'
+
+export default function Checkbox({checked, id, label, onValueChanged, width}: CheckboxParams) {
+    width ??= "w-64"
+
+    id ??= label
+
+    const change = (e: ChangeEvent<HTMLInputElement>) => {
+        onValueChanged?.(e.currentTarget.checked)
+    }
+
+    return <div className={`flex flex-row ${width} m-2`}>
+        <label htmlFor={id} className="text-slate-950 dark:text-slate-50 flex-1">{label}</label>
+        <input type="checkbox" value={checked} onChange={change}/>
+    </div>
+}
+
+export interface CheckboxParams {
+    checked: boolean
+    label: string
+    id?: string
+    onValueChanged?: (checked: boolean) => void
+    width?: string
+}
--- src/components/palette-settings.tsx
+++ src/components/palette-settings.tsx
@@ -1,5 +1,6 @@
 import ColorPalette from '../data/color-palette.ts'
 import RangeSlider from './input/range-slider.tsx'
+import Checkbox from './input/checkbox.tsx'
 
 export default function PaletteSettings({palette, onPaletteChanged}: PaletteSettingsParams) {
     const minLightnessChanged = (minLightness: number) => {
@@ -34,6 +35,42 @@ export default function PaletteSettings({palette, onPaletteChanged}: PaletteSett
         onPaletteChanged?.(cloned)
     }
 
+    const lightnessChanged = (uniformLightness: number) => {
+        const cloned = structuredClone(palette) as ColorPalette
+
+        cloned.minLightness = uniformLightness
+        cloned.maxLightness = uniformLightness
+
+        onPaletteChanged?.(cloned)
+    }
+
+    const chromaChanged = (uniformChroma: number) => {
+        const cloned = structuredClone(palette) as ColorPalette
+
+        cloned.minChroma = uniformChroma
+        cloned.maxChroma = uniformChroma
+
+        onPaletteChanged?.(cloned)
+    }
+
+    const uniformLightnessChanged = (uniformLightness: boolean) => {
+        const cloned = structuredClone(palette) as ColorPalette
+
+        cloned.uniformLightness = uniformLightness
+        cloned.maxLightness = cloned.minLightness
+
+        onPaletteChanged?.(cloned)
+    }
+
+    const uniformChromaChanged = (uniformChroma: boolean) => {
+        const cloned = structuredClone(palette) as ColorPalette
+
+        cloned.uniformLightness = uniformChroma
+        cloned.maxChroma = cloned.minChroma
+
+        onPaletteChanged?.(cloned)
+    }
+
     const shadesChanged = (shades: number) => {
         const cloned = structuredClone(palette) as ColorPalette
 
@@ -44,8 +81,10 @@ export default function PaletteSettings({palette, onPaletteChanged}: PaletteSett
 
     return <div className="flex flex-col">
         <RangeSlider key="shades" value={palette.shades} label="Shades" min={0} max={32} step={1} onValueChanged={shadesChanged}/>
+        <Checkbox checked={palette.uniformLightness} label="Uniform Lightness" onValueChanged={uniformLightnessChanged}/>
         <RangeSlider key="minLightness" value={palette.minLightness} label="Min Lightness" min={0} max={1} step={0.001} decimalPlaces={3} onValueChanged={minLightnessChanged}/>
         <RangeSlider key="maxLightness" value={palette.maxLightness} label="Max Lightness" min={0} max={1} step={0.001} decimalPlaces={3} onValueChanged={maxLightnessChanged}/>
+        <Checkbox checked={palette.uniformChroma} label="Uniform Chroma" onValueChanged={uniformChromaChanged}/>
         <RangeSlider key="minChroma" value={palette.minChroma} label="Min Chroma" min={0.0} max={0.37} step={0.001} decimalPlaces={3} onValueChanged={minChromaChanged}/>
         <RangeSlider key="maxChroma" value={palette.maxChroma} label="Max Chroma" min={0.0} max={0.37} step={0.001} decimalPlaces={3} onValueChanged={maxChromaChanged}/>
     </div>
